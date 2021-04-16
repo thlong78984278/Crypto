232f86a2802a5a570727e6a8136a21f156d8e5a1
Amatoru <62116065+Amatoru@users.noreply.github.com>
2021-04-16 17:03:26 +0700

    Cleaning my trash

--- turing_machine/turing_machine.py
+++ turing_machine/turing_machine.py
@@ -1,5 +1,4 @@
 class Tape(object):
-
     blank_symbol = " "
 
     def __init__(self,
@@ -27,7 +26,6 @@ class Tape(object):
     def __setitem__(self, pos, char):
         self.__tape[pos] = char
 
-
 class TuringMachine(object):
 
     def __init__(self,
@@ -37,33 +35,33 @@ class TuringMachine(object):
                  final_states = None,
                  transition_function = None,
                  reject_state = "NaN"):
-        self.__tape = Tape(tape)
-        self.__head_position = 0
-        self.__blank_symbol = blank_symbol
-        self.__current_state = initial_state
-        self.__reject_state = reject_state
+        self.tape = Tape(tape)
+        self.head_position = 0
+        self.blank_symbol = blank_symbol
+        self.current_state = initial_state
+        self.reject_state = reject_state
         if transition_function == None:
-            self.__transition_function = {}
+            self.transition_function = {}
         else:
-            self.__transition_function = transition_function
+            self.transition_function = transition_function
         if final_states == None:
-            self.__final_states = set()
+            self.final_states = set()
         else:
-            self.__final_states = set(final_states)
+            self.final_states = set(final_states)
 
     def get_current_state(self):
-        return str(self.__current_state)
+        return str(self.current_state)
 
     def get_tape(self):
-        return str(self.__tape)
+        return str(self.tape)
 
     def print_current_state_and_tape(self):
         # print current state and the tape
         state_string = '(' + self.get_current_state() + ')'
         print("{0: <7}▷".format(state_string), end=" ")
         for i in range(len(self.get_tape()) + 1):
-            c = self.__tape[i]
-            if i == self.__head_position:
+            c = self.tape[i]
+            if i == self.head_position:
                 print('{}_'.format(c), end="")
             else:
                 print(c, end="")
@@ -71,31 +69,30 @@ class TuringMachine(object):
 
     def step(self):
         self.print_current_state_and_tape()
-        char_under_head = self.__tape[self.__head_position]
-        x = (self.__current_state, char_under_head)
-        if x in self.__transition_function:
-            y = self.__transition_function[x]
-            self.__tape[self.__head_position] = y[1]
+        char_under_head = self.tape[self.head_position]
+        x = (self.current_state, char_under_head)
+        if x in self.transition_function:
+            y = self.transition_function[x]
+            self.tape[self.head_position] = y[1]
             if y[2] == "R":
-                self.__head_position += 1
+                self.head_position += 1
             elif y[2] == "L":
-                self.__head_position -= 1
-            self.__current_state = y[0]
+                self.head_position -= 1
+            self.current_state = y[0]
         else:
-            print('Error: "{}, {}" is not recognizable'.format(self.__current_state, char_under_head))
-            self.__current_state = self.__reject_state
+            print('Error: "{}, {}" is not recognizable'.format(self.current_state, char_under_head))
+            self.current_state = self.reject_state
 
     def final(self):
-        if self.__current_state == self.__reject_state:
+        if self.current_state == self.reject_state:
             return True
-        if self.__current_state in self.__final_states:
+        if self.current_state in self.final_states:
             self.print_current_state_and_tape()
             return True
         else:
             return False
 
-
-class TuringMachine2(object):
+class TuringMachine2(TuringMachine):
     def __init__(self,
                  tape = "",
                  blank_symbol = " ",
@@ -103,63 +100,22 @@ class TuringMachine2(object):
                  final_states = None,
                  transition_function = None,
                  reject_state = "NaN"):
-        self.__tape = Tape(tape)
-        self.__head_position = 0
-        self.__blank_symbol = blank_symbol
-        self.__current_state = initial_state
-        self.__reject_state = reject_state
-        if transition_function == None:
-            self.__transition_function = {}
-        else:
-            self.__transition_function = transition_function
-        if final_states == None:
-            self.__final_states = set()
-        else:
-            self.__final_states = set(final_states)
-
-    def get_current_state(self):
-        return str(self.__current_state)
-
-    def get_tape(self):
-        return str(self.__tape)
-
-    def print_current_state_and_tape(self):
-        # print current state and the tape
-        state_string = '(' + self.get_current_state() + ')'
-        print("{0: <7}▷".format(state_string), end=" ")
-        for i in range(len(self.get_tape()) + 1):
-            c = self.__tape[i]
-            if i == self.__head_position:
-                print('{}_'.format(c), end="")
-            else:
-                print(c, end="")
-        print()
-
+        super(TuringMachine2, self).__init__(tape, blank_symbol, initial_state, final_states, transition_function, reject_state)
 
     def step(self):
         self.print_current_state_and_tape()
-        char_under_head = self.__tape[self.__head_position]
-        x = (self.__current_state, char_under_head)
-        if x in self.__transition_function:
-            y = self.__transition_function[x]
+        char_under_head = self.tape[self.head_position]
+        x = (self.current_state, char_under_head)
+        if x in self.transition_function:
+            y = self.transition_function[x]
             # change a little bit of code here, is that ok? Idk But let's try
             if y[1] == "R":
-                self.__head_position += 1
-            elif y[1] == "L" and self.__head_position > 0:
-                self.__head_position -= 1
+                self.head_position += 1
+            elif y[1] == "L" and self.head_position > 0:
+                self.head_position -= 1
             elif y[1] != "R" and y[1] != "L":
-                self.__tape[self.__head_position] = y[1]
-            self.__current_state = y[0]
+                self.tape[self.head_position] = y[1]
+            self.current_state = y[0]
         else:
-            print('Error: "{}, {}" is not recognizable'.format(self.__current_state, char_under_head))
-            self.__current_state = self.__reject_state
-
-
-    def final(self):
-        if self.__current_state == self.__reject_state:
-            return True
-        if self.__current_state in self.__final_states:
-            self.print_current_state_and_tape()
-            return True
-        else:
-            return False
+            print('Error: "{}, {}" is not recognizable'.format(self.current_state, char_under_head))
+            self.current_state = self.reject_state
